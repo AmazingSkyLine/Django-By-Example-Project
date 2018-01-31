c7c50cf2cce38c820c2e6f341951771a640e2fd8
AmazingSkyLine <15002963138@163.com>
2018-01-31 16:49:10 +0800

    Update settings.py
    Delete DataBase Info

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite',
-        'USER': 'www-data',
-        'PASSWORD': 'xm19971215',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
